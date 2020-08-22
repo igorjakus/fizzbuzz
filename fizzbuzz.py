36cdd34fd60191cec09884a77fd8e340a193e141
igorjakus <igorjakus3@gmail.com>
2020-08-22 21:02:02 +0200

    Added solution in Python

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,10 +5,6 @@ for number in range(1, 101):
         output += 'Fizz'
     if number % 5 == 0:
         output += 'Buzz'
-    if number % 7 == 0:
-        output += 'Bizz'
-    if number % 9 == 0:
-        output += 'Fuzz'
 
     if not output:
         output += str(number)
